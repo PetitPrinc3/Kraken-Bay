4f40c8dc9ad2c0b28b10518979365f4181acc4bb
PetitPrinc3 <arthur.reppelin@gmail.com>
2024-05-29 16:12:58 -0400

    adminStuff

--- devTools.py
+++ devTools.py
@@ -3,60 +3,13 @@ from json import loads, dump
 from urllib.request import urlretrieve
 from PythonModules.krakenConf import *
 from PythonModules.mkvInfo import *
+import mysql.connector
 
 ASSETS_PATH = "Kraken - Web/public/Assets/"
 JSON_PATH = ASSETS_PATH + "JSON_DMPS/"
 MOVIES_PATH = ASSETS_PATH + "Movies/"
 SERIES_PATH = ASSETS_PATH + "Movies/"
-
-def checkImages() :
-    existing_file = []
-    existing_path = []
-
-    list = [_ for _ in os.walk("Images")]
-    list.pop(0)
-
-    used = []
-    images = []
-
-    with open(f"{JSON_PATH}movies.json", 'r', encoding='utf-8') as f:
-        d = loads(f.read())["Titles"]
-        for _ in d:
-            used.append(_['thumbUrl'].split("/")[3])
-            used.append(_["posterUrl"].split("/")[3])
-            images.append(_['thumbUrl'])
-            images.append(_["posterUrl"])
-
-    with open(f"{JSON_PATH}series.json", 'r', encoding='utf-8') as f:
-        d = loads(f.read())["Titles"]
-        for _ in d:
-            used.append(_['thumbUrl'].split("/")[3])
-            used.append(_["posterUrl"].split("/")[3])
-            images.append(_['thumbUrl'])
-            images.append(_["posterUrl"])
-
-    for f in list:
-        if len(f[2]) == 0:
-            os.rmdir(f[0])
-        else :
-            for _ in f[2]:
-                if _ in existing_file:
-                    if f[0] not in used :
-                        os.remove(os.path.join(f[0], _))
-                    elif existing_path[existing_file.index(_)] not in used:
-                        os.remove(os.path.join(existing_path[existing_file.index(_)], _))                                   
-                else :
-                    existing_file.append(_)
-                    existing_path.append(f[0])
-
-
-    for _ in images:
-        pth = _.split("/Assets/")[1]
-        if not os.path.exists(pth):
-            p_src = 'http://image.tmdb.org/t/p/w500/' + pth.split("/")[-1]
-            if not os.path.exists(os.path.join("Images", pth.split("/")[-2])):
-                os.mkdir(os.path.join("Images", pth.split("/")[-2]))
-            urlretrieve(p_src, os.path.join("Images", pth.split("/")[-2], pth.split("/")[-1]))
+IMAGE_PATH = ASSETS_PATH + "Images/"
 
 def resetGenres() :
     genres = []
@@ -147,6 +100,154 @@ def addSubTrack():
     with open(f"{JSON_PATH}series.json", "w", encoding="utf-8") as new:
         dump(newTitles, new, ensure_ascii=False, indent=4)
 
+def remakeSeasons():
+    with open(f"{JSON_PATH}series.json", "r", encoding="utf-8") as old:
+        movies = loads(old.read())["Titles"]
+
+    newTitles = loads('{ "Titles": [] }')
+
+    for movie in movies :
+
+        client = mysql.connector.connect(host = hostname, user=username, passwd = password, database = database)
+        cursor = client.cursor()
+        cursor.execute(f"""SELECT id FROM Media WHERE videoUrl="{movie["videoUrl"]}" """)
+        serieId = cursor.fetchall()[0][0]
+        sql = f"""select season from Serie_EP where serieId="{serieId}" group by season order by season"""
+        cursor.execute(sql)
+        seasons = ",".join([_[0] for _ in cursor.fetchall()])
+
+        newJson ={
+                "title": movie["title"],
+                "altTitle": movie["altTitle"],
+                "type": movie["type"],
+                "description": movie["description"],
+                "videoUrl": movie["videoUrl"],
+                "thumbUrl": movie["thumbUrl"],
+                "posterUrl": movie["posterUrl"],
+                "seasons": seasons,
+                "duration": movie["duration"],
+                "languages": movie["languages"],
+                "subtitles": movie["subtitles"],
+                "genre": movie["genre"]
+            }
+
+        newTitles["Titles"].append(newJson)
+
+    with open(f"{JSON_PATH}series.json", "w", encoding="utf-8") as new:
+        dump(newTitles, new, ensure_ascii=False, indent=4)
+
+def remakeVideo():
+    with open(f"{JSON_PATH}series.json", "r", encoding="utf-8") as old:
+        movies = loads(old.read())["Titles"]
+
+    newTitles = loads('{ "Titles": [] }')
+
+    for movie in movies :
+
+        client = mysql.connector.connect(host = hostname, user=username, passwd = password, database = database)
+        cursor = client.cursor()
+        cursor.execute(f"""SELECT id, seasons FROM Media WHERE videoUrl="{movie["videoUrl"]}" """)
+        res = cursor.fetchall()
+        serieId = res[0][0]
+        seasons = res[0][1].split(",")[0]
+        sql = f"""select videoUrl from Serie_EP where serieId="{serieId}" and season="{seasons}" order by episode"""
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        if len(res) == 0:
+            print(sql)
+        else:
+            video = res[0][0]
+
+        newJson ={
+                "title": movie["title"],
+                "altTitle": movie["altTitle"],
+                "type": movie["type"],
+                "description": movie["description"],
+                "videoUrl": video,
+                "thumbUrl": movie["thumbUrl"],
+                "posterUrl": movie["posterUrl"],
+                "seasons": movie["seasons"],
+                "duration": movie["duration"],
+                "languages": movie["languages"],
+                "subtitles": movie["subtitles"],
+                "genre": movie["genre"]
+            }
+
+        newTitles["Titles"].append(newJson)
+
+    with open(f"{JSON_PATH}series.json", "w", encoding="utf-8") as new:
+        dump(newTitles, new, ensure_ascii=False, indent=4)
+
+def renameEps():
+    dmps = os.listdir(JSON_PATH)
+    dmps.pop(dmps.index("movies.json"))
+    dmps.pop(dmps.index("series.json"))
+    dmps.pop(dmps.index("genres.json"))
+    for seso in dmps:
+        with open(f'{JSON_PATH}{seso}', 'r', encoding='utf-8') as old:
+            eps = loads(old.read())
+        new = loads('{"Titles": []}')
+        for ep in eps["Titles"]:
+            old_t = ep["title"]
+            ep_n = str(int(old_t.split(",")[-1].split(" ")[-1]) + 1)
+            new_t = ",".join(old_t.split(",")[:-1]) + f", Ep {ep_n}"
+            new["Titles"].append({
+            "title": new_t,
+            "season": ep["season"],
+            "episode": ep_n,
+            "videoUrl": ep["videoUrl"]
+        })
+        with open(f'{JSON_PATH}{seso}', 'w', encoding='utf-8') as old:
+            dump(new, old, ensure_ascii=False, indent=4)
+
+def checkFolders():
+    list = [_ for _ in os.walk(IMAGE_PATH)][0][1]
+
+    used = []
+    imgs = []
+
+    try:
+        with open(f"{JSON_PATH}movies.json", 'r', encoding='utf-8') as f:
+            d = loads(f.read())["Titles"]
+            for _ in d:
+                used.append(_['thumbUrl'].split("/")[-2])
+                imgs.append((_['thumbUrl'], _['posterUrl']))
+    except :
+        pass
+
+    try:
+        with open(f"{JSON_PATH}series.json", 'r', encoding='utf-8') as f:
+            d = loads(f.read())["Titles"]
+            for _ in d:
+                used.append(_['thumbUrl'].split("/")[-2])
+                imgs.append((_['thumbUrl'], _['posterUrl']))
+    except :
+        pass
+
+    for _ in list :
+        if _ not in used:
+            info(f"Removing {_}")
+            for i in os.listdir(os.path.join(IMAGE_PATH, _)):
+                os.remove(os.path.join(IMAGE_PATH, _, i))
+            os.rmdir(os.path.join(IMAGE_PATH, _))
+        if len(os.listdir(os.path.join(IMAGE_PATH, _))) == 0:
+            os.rmdir(os.path.join(IMAGE_PATH, _))
+            info(f"Dir {_} was empty.")
+            list.pop(list.index(_))
+    
+    for _ in used :
+        if _ not in list:
+            warning(f"Missing {_}")
+            info("Missing imaages :")
+            i_ = imgs[used.index(_)]
+            os.makedirs(os.path.join(IMAGE_PATH, _))
+            t_src = 'http://image.tmdb.org/t/p/w500/' + os.path.basename(i_[0])
+            urlretrieve(t_src, os.path.join(IMAGE_PATH, _, os.path.basename(i_[0])))
+            info(f"Fetched {os.path.basename(i_[0])}")
+            p_src = 'http://image.tmdb.org/t/p/w500/' + os.path.basename(i_[1])
+            urlretrieve(p_src, os.path.join(IMAGE_PATH, _, os.path.basename(i_[1])))
+            info(f"Fetched {os.path.basename(i_[1])}")
+
 if __name__ == "__main__":
     print(f'$$ DEV $$\n')
     print(f'JSON PATH : {JSON_PATH}')
@@ -154,13 +255,26 @@ if __name__ == "__main__":
 Options :
 1) checkImages
 2) resetGenres
-3) addSubTracks""")
+3) addSubTracks
+4) remakeSeasons
+5) remakeVideo
+6) renameEps
+7) checkFolders
+          """)
     choice = input(">>>")
     match choice:
         case "1" :
-            checkImages()
+            pass
         case "2" :
             resetGenres()
         case "3" :
             addSubTrack()
+        case "4" :
+            remakeSeasons()
+        case "5":
+            remakeVideo()
+        case "6":
+            renameEps()
+        case "7" :
+            checkFolders()
     exit()
