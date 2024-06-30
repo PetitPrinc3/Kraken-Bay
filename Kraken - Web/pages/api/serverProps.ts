d7b13a1e093e4111d9a456d6798c42662d877dc0
PetitPrinc3 <arthur.reppelin@gmail.com>
2024-06-29 20:02:21 -0400

    remove cmd execution

--- Kraken - Web/pages/api/serverProps.ts
+++ Kraken - Web/pages/api/serverProps.ts
@@ -38,9 +38,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const os = require("os")
             const execSync = require('child_process').execSync
 
-            const hotSpot = execSync("cmd", { encoding: 'utf-8' }).split("\r\n")[0]
-            const smbStatus = execSync("cmd", { encoding: 'utf-8' }).split("\r\n")[0]
-
             const serverProps = {
                 osUptime: os.uptime(),
                 osPlatform: process.platform,
